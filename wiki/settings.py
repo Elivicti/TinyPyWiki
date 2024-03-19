2da9684bf56231c6f42b53369d4eea6248672199
Elivicti <edwynb72@gmail.com>
2024-03-19 16:45:39 +0800

    加载wiki_editor应用，为所有模板默认加载static标签

--- wiki/settings.py
+++ wiki/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 	'mainwiki',
+	'wiki_editor',
 	'user_system',
 	'sslserver',
 ]
@@ -70,6 +71,9 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+            ],
+            'builtins': [
+                'django.templatetags.static',
             ],
         },
     },
