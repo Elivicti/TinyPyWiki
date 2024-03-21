61efea4edde1d7c9d5a78e6df5d88d3d7d60dc18
Elivicti <edwynb72@gmail.com>
2024-03-21 18:30:14 +0800

    页面跳转字段更改为next，页面中添加csrf_token

--- user_system/views.py
+++ user_system/views.py
@@ -1,22 +1,20 @@
 from django.shortcuts import render, redirect
-from django.views.decorators.csrf import csrf_exempt
 from django.utils.safestring import SafeText
 from . import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.core.exceptions import ValidationError
 
 # Create your views here.
 
-@csrf_exempt
+
 def user_login(request):
 	if request.method == "GET":
 		return render(request, "login.html", { "form" : forms.WikiUserLogin() })
 	
-	prev = request.POST.get('prev', '/')
-	if (prev == "/user/register/"):
-		prev = "/"
+	next = request.POST.get('next', '/')
+	if (next == "/user/register/" or next == ""):
+		next = "/"
 
 	email = request.POST["email"]
 	password = request.POST["password"]
@@ -31,30 +29,28 @@ def user_login(request):
 
 	login(request, user)
 
-	return redirect(prev)
+	return redirect(next)
 
-@csrf_exempt
 def user_logout(request):
 	next_page = request.META.get("HTTP_REFERER")
 	logout(request)
 	return redirect(request.META.get("HTTP_REFERER"))
 
-@csrf_exempt
 def register(request):
 	if request.method == "GET":
 		form = forms.WikiUserRegister()
 		return render(request, "register.html", { "form" : form })
 	
-	prev = request.POST.get('prev', '/')
-	if (prev == "/user/login/"):
-		prev = "/"
+	next = request.POST.get('next', '/')
+	if (next == "/user/login/" or next == ""):
+		next = "/"
 
 	form = forms.WikiUserRegister(data=request.POST)
 	if form.check():
 		form.save()
 		user = authenticate(request, username=request.POST["username"], password=request.POST["password"])
 		login(request, user)
-		return redirect(prev)
+		return redirect(next)
 	
 	return render(request, "register.html", {
 		"form" : form,
