c711226a362558a2a10c015dc73639688102b1bd
Elivicti <edwynb72@gmail.com>
2024-03-21 18:28:54 +0800

    添加页面右上侧的面板生成器

---
+++ mainwiki/utils.py
@@ -0,0 +1,21 @@
+
+SECTIONS = [ "read", "edit", "history" ]
+SECTION_NAMES = dict(zip(SECTIONS, ["Read", "Edit", "View History"]))
+
+class SectionTab:
+	def __init__(self, href: str, id: str, active: bool) -> None:
+		self.href = "#" if active else href
+		self.id = id
+		self.title = SECTION_NAMES[id]
+		self.cls = "active" if active else ""
+
+def get_section_tab(request, article_title, active_section: str, force_hide_editor: bool = False) -> list[SectionTab]:
+	ret = []
+	for section in SECTIONS:
+		if (not request.user.is_authenticated or force_hide_editor) and section == "edit":
+			continue
+		ret.append(SectionTab(
+			"/a/{}/{}".format(article_title, section if section != "read" else ""),
+			section, section == active_section
+		))
+	return ret
