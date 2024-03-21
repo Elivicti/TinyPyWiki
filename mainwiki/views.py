0f8dbb0a19f6a70e1543d66132d17e78a7d01dc5
Elivicti <edwynb72@gmail.com>
2024-03-21 18:29:10 +0800

    从mainwiki移除显示文章的视图函数

--- mainwiki/views.py
+++ mainwiki/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render, redirect
 from django.urls import path
-from wiki_editor.mdhtml import parse_markdown
-from django.utils.safestring import SafeText
 
 # 创建视图处理函数
 def home(request):
@@ -15,17 +13,3 @@ def test_page(request):
         "article_content": "this is content"
     })
 
-
-def get_article(request, article_id: str):
-    try:
-        with open("assets/articles/%s.md" % article_id, "r") as f:
-            article = parse_markdown(f)
-    except:
-        return redirect("/404/")
-    else:
-        return render(request, "base.html", {
-            "var_title": article.title,
-            "var_intro": SafeText(article.intro),
-            "var_contents": SafeText(article.contents_panel),
-            "var_article": SafeText(article.article),
-        })
