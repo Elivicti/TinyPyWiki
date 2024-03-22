72981279919c524ed0f1fc0cc1997d3ab11786ee
Elivicti <edwynb72@gmail.com>
2024-03-22 10:18:26 +0800

    规范化代码

--- wiki_talk/views.py
+++ wiki_talk/views.py
@@ -11,25 +11,19 @@ def show_talk(request):
 	if request.method == "GET":
 		messages = TalkMessage.objects.filter().order_by("-edit_time")
 	
-	
 		form = forms.TalkMessageForm(initial={"user": request.user})
 		return render(request, "talk.html", {
-		"hide_external_links": True,
-		"talk_page": "Talk",
-		"tab_sections": get_section_tab(request, None, "read", force_hide_editor=True, force_hide_history=True),
-		"messages": messages,
-		"var_last_edit_time": messages[0].edit_time,
-		"form": form
+			"hide_external_links": True,
+			"talk_page": "Talk",
+			"tab_sections": get_section_tab(request, None, "read", force_hide_editor=True, force_hide_history=True),
+			"messages": messages,
+			"var_last_edit_time": messages[0].edit_time,
+			"form": form
 		})
 	
-	form = forms.TalkMessageForm(initial={"user": request.user},data=request.POST)
+	form = forms.TalkMessageForm(initial={"user": request.user}, data=request.POST)
 	form.full_clean()
 	if form.is_valid():
 		form.save()
 		return redirect("/talk/")
 
-
-def func(request):
-	request.user
-
-	pass
