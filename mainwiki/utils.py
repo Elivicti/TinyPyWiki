3824752d3929362f67499b96b18d7532c1a15289
Elivicti <edwynb72@gmail.com>
2024-03-22 09:49:03 +0800

    section_tab可选隐藏历史

--- mainwiki/utils.py
+++ mainwiki/utils.py
@@ -9,10 +9,14 @@ class SectionTab:
 		self.title = SECTION_NAMES[id]
 		self.cls = "active" if active else ""
 
-def get_section_tab(request, article_title, active_section: str, force_hide_editor: bool = False) -> list[SectionTab]:
+def get_section_tab(request, article_title, active_section: str,
+					force_hide_editor: bool = False,
+					force_hide_history: bool = False) -> list[SectionTab]:
 	ret = []
 	for section in SECTIONS:
 		if (not request.user.is_authenticated or force_hide_editor) and section == "edit":
+			continue
+		if section == "history" and force_hide_history:
 			continue
 		ret.append(SectionTab(
 			"/a/{}/{}".format(article_title, section if section != "read" else ""),
